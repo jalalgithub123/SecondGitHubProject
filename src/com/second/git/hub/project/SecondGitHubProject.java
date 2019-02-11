c7b4036925236f6f43f108c47cf0ce2a1b9e1fc1
jalal8068 <jalalahamad9@gmail.com>
2019-02-11 21:28:54 +0530

    2nd project correction

--- src/com/second/git/hub/project/SecondGitHubProject.java
+++ src/com/second/git/hub/project/SecondGitHubProject.java
@@ -4,7 +4,7 @@ public class SecondGitHubProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Git hub 2nd project");
+System.out.println("Git hub 2nd project correction");
 	}
 
 }
