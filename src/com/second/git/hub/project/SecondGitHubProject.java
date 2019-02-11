975e45234cc55482b13b04abd943758ae5159eba
jalal8068 <jalalahamad9@gmail.com>
2019-02-11 21:27:41 +0530

    2nd git hub project

---
+++ src/com/second/git/hub/project/SecondGitHubProject.java
@@ -0,0 +1,10 @@
+package com.second.git.hub.project;
+
+public class SecondGitHubProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Git hub 2nd project");
+	}
+
+}
